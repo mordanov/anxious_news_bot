0b1c9714078fb0b44b6aeaf92e2fa582d5e71053
Aleksandr Mordanov <aleksandr.mordanov.external@zalando.de>
2026-08-16 20:40:07 +0200

    formatting

--- tests/unit/preferences/test_specify.py
+++ tests/unit/preferences/test_specify.py
@@ -128,7 +128,12 @@ class InspectingInterpreter(DeterministicExplicitInterpreter):
         self.histories = []
 
     async def interpret(
-        self, request_id, statement, profile_snapshot, relevant_history, language_code=None
+        self,
+        request_id,
+        statement,
+        profile_snapshot,
+        relevant_history,
+        language_code=None,
     ):
         assert self.repository.transaction_open is False
         self.histories.append(tuple(relevant_history))
@@ -156,7 +161,12 @@ class AdaptiveInterpreter:
         self.calls = 0
 
     async def interpret(
-        self, request_id, statement, profile_snapshot, relevant_history, language_code=None
+        self,
+        request_id,
+        statement,
+        profile_snapshot,
+        relevant_history,
+        language_code=None,
     ):
         del statement, relevant_history
         self.calls += 1
